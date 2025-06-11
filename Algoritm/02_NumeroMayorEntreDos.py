4982e07d4f7381a1f6ce3b722b6d6037b627f754
Pablo <emariveroupc@gmail.com>
2025-06-11 05:40:26 -0300

    suma

---
+++ Algoritm/02_NumeroMayorEntreDos.py
@@ -0,0 +1,10 @@
+# Numero Mayor Entre Dos
+numero_1 = float(input("Escriba el Primer numero: "))
+numero_2 = float(input("Escriba el Segundo numero: "))
+
+if numero_1 > numero_2:
+    print(f"El Numero mayor es {numero_1}")
+elif numero_1 < numero_2: 
+    print(f"El Numero mayor es {numero_2}")
+else:
+    print("Los Numeros Son Iguales")
---
+++ Algoritm/03_SignodeNumeros.py
@@ -0,0 +1,2 @@
+#Signo de Numeros
+#!= significa distinto de  ej: a != b ..osea 4!=5 los numeros son diferentes
